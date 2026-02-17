49b211e042eb0251c735315d0b1a09df73e288c9
mrMohamadpy <malyasy620@gmail.com>
2026-02-17 14:55:03 +0330

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,5 +1,5 @@
 import telebot
-TOKEN = "8001551459:AAG4yghPSN3VdFrc86k4Gq5hS_E6HUwqC2Q" # توکن رباتت از BotFather
+TOKEN = "." # توکن رباتت از BotFather
 bot = telebot.TeleBot(TOKEN)
 دستور /start
 @bot.message_handler(commands=['start'])
